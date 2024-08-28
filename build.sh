49b256e593688c83e8dbef899a0d0ee5a894e19e
bessavagner <bessavagner@gmail.com>
2024-08-28 15:22:35 -0300

    perf: set build stage in order to create styles.css, avoiding css being major language in repository

--- build.sh
+++ build.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
-python -m pip install -r requirements.txt
-npm install -D tailwindcss
-npx tailwindcss -i ./src/input.css -o ./src/output.css
+
+if [ -f .env ]; then
+    export $(grep -v '^#' .env | xargs)
+fi
+
+if [ "$PRODUCTION" -eq 1 ]; then
+    echo "🐋 ⚙️ Buiding docker image for production mode"
+    docker stop sample-chat
+    docker rm sample-chat
+    docker build -t sample_chat --rm=true .
+else
+    echo "🚧 ⚙️ Building assets for development mode"
+    python -m pip install -r requirements.txt
+    npm install -D tailwindcss
+    npx tailwindcss -i ./src/input.css -o ./src/output.css
+fi
--- run.sh
+++ run.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-python app.py
+
+./build.sh
+
+if [ "$PRODUCTION" -eq 1 ]; then
+    echo "🐋 Running container..."
+    docker run -d --name sample-chat -p 8081:8080 -v $PWD:/usr/src sample_chat:latest
+else
+    echo "🚧 ⚙️ Running in development mode"
+    python app.py
+fi
